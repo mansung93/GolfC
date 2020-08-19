b196ee79a3aab66be708830a2bec47fdb25ae5c5
DESKTOP-912HOCH\Green_Computer <mansung93@hotmail.com>
2020-08-19 16:49:00 +0900

    GolferController에 golferList 추가

--- src/main/java/com/ncs/golfc/GolferController.java
+++ src/main/java/com/ncs/golfc/GolferController.java
@@ -1,23 +1,30 @@
 package com.ncs.golfc;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import service.GolferService;
+import vo.GolferVO;
+
 @Controller
 public class GolferController {
-	/*
-	 * @Autowired GolferService golferService;
-	 */
+	
+	@Autowired
+	GolferService service;
 	
 	@RequestMapping(value = "/golferList")
 	public ModelAndView golferList(ModelAndView mv) {
-		/*
-		 * List<GolferVO> list = golferService.selectList(); if (list != null) {
-		 * mv.addObject("ping", list); } else { mv.addObject("message",
-		 * "검색된 자료가 없습니다."); }
-		 */
+		List<GolferVO> list = service.selectList();
+		if (list != null) {
+			mv.addObject("Ping", list);
+		} else {
+			mv.addObject("message", "검색된 자료가 없습니다.");
+		}
 		mv.setViewName("golfer/golferList");
 		return mv;
-	}
+	} // golferList
 }
